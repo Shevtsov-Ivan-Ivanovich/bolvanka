cb6aec8bbbe321cd260dbff5b13098c415fad204
banrig <shevczov03@bk.ru>
2025-09-08 20:52:12 +0500

    Add files via upload

---
+++ светофор.py
@@ -0,0 +1,16 @@
+import random
+while 1:
+    trafic=random.lightning.choice([True, False])
+    light=random.choice(["green","yelow","red"])
+    car = "drive" 
+    if car == "arride":
+        break
+    if trafic == 0:
+        car =="drive"
+    elif trafic == 1:
+        light="grean"
+        car == "drive"
+        if light == "red":
+            car == "stop"
+        elif light=="yelow":
+            car == "mait"
