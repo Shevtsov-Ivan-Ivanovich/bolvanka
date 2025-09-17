3e8fe29ee08b238959530bd9cf93a20d47a023bb
banrig <shevczov03@bk.ru>
2025-09-17 20:53:17 +0500

    Add files via upload

---
+++ Solnumbr.py/Solnumbr.py/Solnumbr.py
@@ -0,0 +1,11 @@
+
+class sorted:
+    def sort(array,sort):
+      if sort =="1":
+         return sorted(array)
+      elif sort == "2":
+         return sorted(array, reverse=True)
+      
+    print(sort([1,5,8,2,6,3,7,9,4],str(input("tup(1,2)"))))
+
+      
---
+++ Solnumbr.py/Solnumbr.py/Solplay.py
@@ -0,0 +1,3 @@
+
+import Solnumbr
+print(sort([1,5,8,2,6,3,7,9,4],str(input("tup(1,2)"))))`
